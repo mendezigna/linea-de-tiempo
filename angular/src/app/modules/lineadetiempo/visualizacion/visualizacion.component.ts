6a71d093d63e63feea2c64e3740c889b64e5de42
DuglasColoma <duglas.espanol@gmail.com>
2021-09-21 18:54:53 -0300

    fixing un bug con formularios sin dias ni meses

--- angular/src/app/modules/lineadetiempo/visualizacion/visualizacion.component.ts
+++ angular/src/app/modules/lineadetiempo/visualizacion/visualizacion.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
-import { Fecha, LineaDeTiempo, Punto } from '../../utils/LineaDeTiempo';
+import { Component, Input, OnInit }     from '@angular/core';
+import { NgbCarouselConfig }            from '@ng-bootstrap/ng-bootstrap';
+import { Fecha, LineaDeTiempo, Punto }  from '../../utils/LineaDeTiempo';
 
 @Component({
-  selector: 'app-visualizacion',
+  selector:    'app-visualizacion',
   templateUrl: './visualizacion.component.html',
-  styleUrls: ['./visualizacion.component.css'],
+  styleUrls:  ['./visualizacion.component.css'],
 })
 export class VisualizacionComponent implements OnInit {
 
@@ -14,29 +14,29 @@ export class VisualizacionComponent implements OnInit {
 
   constructor(config: NgbCarouselConfig) {
     config.showNavigationIndicators = false
-    config.showNavigationArrows = true
-    config.wrap = false
+    config.showNavigationArrows     = true
+    config.wrap                     = false
   }
 
   ngOnInit(): void {
   }
   puntosOrdenados() {
     const puntosDC = this.lineaDeTiempo.puntos.filter(punto => punto.fecha.dc)
-      .sort((a, b) => (a.fecha.dia.valueOf()) - (b.fecha.dia.valueOf()))
-      .sort((a, b) => (a.fecha.mes.valueOf()) - (b.fecha.mes.valueOf()))
-      .sort((a, b) => (a.fecha.anho.valueOf()) - (b.fecha.anho.valueOf()))
+      .sort((a, b) => (a.fecha.dia.valueOf()||0)  - (b.fecha.dia.valueOf() ||0))
+      .sort((a, b) => (a.fecha.mes.valueOf()||0)  - (b.fecha.mes.valueOf() ||0))
+      .sort((a, b) => (a.fecha.anho.valueOf())    - (b.fecha.anho.valueOf()   ))
     const puntosAC = this.lineaDeTiempo.puntos.filter(punto => !punto.fecha.dc)
-      .sort((a, b) => (a.fecha.dia.valueOf()) - (b.fecha.dia.valueOf()))
-      .sort((a, b) => (a.fecha.mes.valueOf()) - (b.fecha.mes.valueOf()))
-      .sort((a, b) => (b.fecha.anho.valueOf()) - (a.fecha.anho.valueOf()))
+      .sort((a, b) => (a.fecha.dia.valueOf()||0)  - (b.fecha.dia.valueOf() ||0))
+      .sort((a, b) => (a.fecha.mes.valueOf()||0)  - (b.fecha.mes.valueOf() ||0))
+      .sort((a, b) => (b.fecha.anho.valueOf())    - (a.fecha.anho.valueOf()   ))
     return puntosAC.concat(puntosDC)
   }
 
   fechaFormateada(fecha : Fecha) {
     const anho = fecha.anho
-    const mes = fecha.mes ? fecha.mes : "XX" 
-    const dia = fecha.dia ? fecha.dia : "XX" 
-    const dc = fecha.dc ? "DC" : "AC" 
+    const mes  = fecha.mes ? fecha.mes : "XX" 
+    const dia  = fecha.dia ? fecha.dia : "XX" 
+    const dc   = fecha.dc  ?       "DC" : "AC" 
 
     return `${anho} - ${mes} - ${dia} ${dc}`
   }
