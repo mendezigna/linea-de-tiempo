efb690cec2935a6b88c22ee25a42d4ad4667b9cc
nacho <ignacio.mendez0000@gmail.com>
2021-09-22 16:59:40 -0300

    arreglo de año bisiesto

--- angular/src/app/modules/lineadetiempo/visualizacion/visualizacion.component.ts
+++ angular/src/app/modules/lineadetiempo/visualizacion/visualizacion.component.ts
@@ -12,7 +12,7 @@ export class VisualizacionComponent implements OnInit {
   @Input('linea-de-tiempo')
   lineaDeTiempo!: LineaDeTiempo;
 
-  fechasPorPuntos : Map<string, Punto[]> = new Map()
+  fechasPorPuntos: Map<string, Punto[]> = new Map()
 
   constructor(config: NgbCarouselConfig) {
     config.showNavigationIndicators = false
@@ -24,13 +24,13 @@ export class VisualizacionComponent implements OnInit {
   }
   puntosOrdenados() {
     const puntosDC = this.lineaDeTiempo.puntos.filter(punto => punto.fecha.dc)
-      .sort((a, b) => (a.fecha.dia.valueOf() || 0) - (b.fecha.dia.valueOf() || 0))
-      .sort((a, b) => (a.fecha.mes.valueOf() || 0) - (b.fecha.mes.valueOf() || 0))
-      .sort((a, b) => (a.fecha.anho.valueOf()    ) - (b.fecha.anho.valueOf()    ))
+      .sort((a, b) => (a.fecha.dia ? a.fecha.dia.valueOf() : 0) - (b.fecha.dia ? a.fecha.dia.valueOf() : 0))
+      .sort((a, b) => (a.fecha.mes ? a.fecha.mes.valueOf() : 0) - (b.fecha.mes ? a.fecha.mes.valueOf() : 0))
+      .sort((a, b) => (a.fecha.anho ? a.fecha.anho.valueOf() : 0) - (b.fecha.anho ? b.fecha.anho.valueOf() : 0))
     const puntosAC = this.lineaDeTiempo.puntos.filter(punto => !punto.fecha.dc)
-      .sort((a, b) => (a.fecha.dia.valueOf() || 0) - (b.fecha.dia.valueOf() || 0))
-      .sort((a, b) => (a.fecha.mes.valueOf() || 0) - (b.fecha.mes.valueOf() || 0))
-      .sort((a, b) => (b.fecha.anho.valueOf()    ) - (a.fecha.anho.valueOf()    ))
+      .sort((a, b) => (a.fecha.dia ? a.fecha.dia.valueOf() : 0) - (b.fecha.dia ? a.fecha.dia.valueOf() : 0))
+      .sort((a, b) => (a.fecha.mes ? a.fecha.mes.valueOf() : 0) - (b.fecha.mes ? a.fecha.mes.valueOf() : 0))
+      .sort((a, b) => (b.fecha.anho ? b.fecha.anho.valueOf() : 0) - (a.fecha.anho ? a.fecha.anho.valueOf() : 0))
     return puntosAC.concat(puntosDC)
   }
 
@@ -50,14 +50,14 @@ export class VisualizacionComponent implements OnInit {
 
   puntosPorFecha() {
     const puntos = this.puntosOrdenados()
-    const fechasPorPuntos : {fecha: string, puntos : Punto[]}[] = []
+    const fechasPorPuntos: { fecha: string, puntos: Punto[] }[] = []
     puntos.forEach(punto => {
       const formateada = this.fechaFormateada(punto.fecha)
       const fechaPorPunto = fechasPorPuntos.find(fechaPorPunto => fechaPorPunto.fecha == formateada)
-      if(fechaPorPunto){
+      if (fechaPorPunto) {
         fechaPorPunto.puntos.push(punto)
       } else {
-        fechasPorPuntos.push({fecha: formateada, puntos : [punto]})
+        fechasPorPuntos.push({ fecha: formateada, puntos: [punto] })
       }
     })
     return fechasPorPuntos
