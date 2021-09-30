3a9323b2b6f26db01ed2ea3352d9e6fff6969323
nacho <ignacio.mendez0000@gmail.com>
2021-09-30 20:22:34 -0300

    tema obscuro

--- angular/src/app/modules/ui/themes.ts
+++ angular/src/app/modules/ui/themes.ts
@@ -1,13 +1,27 @@
 export const themes : any = {
     lightTheme: {
-        '--background': 'aliceblue',
-        '--text-color': '#0F0F0F',
-        '--tasks-background': 'cornflowerblue'
+        '--navbar-color': '#6264b4',
+        '--page-background': '#F7F7F7',
+        '--content-background': 'white',
+        '--content-border': '#DFDFDF',
+        '--navbar-button-color': 'rgb(85, 80, 144)',
+        '--navbar-button-border': 'rgba(53, 28, 112, 0.301)',
+        '--navbar-button-hover-color': 'rgb(147, 143, 192)',
+        '--navbar-button-hover-border': 'rgba(182, 178, 192, 0.301)',
+        '--font-color': '#f1f1f1',
+        '--icon-color': '#f1f1f1',
     },
     darkTheme: {
-        '--background': 'purple',
-        '--text-color': 'whitesmoke',
-        '--tasks-background': 'rebeccapurple'
+        '--navbar-color': '#6264b4',
+        '--page-background': '#5c5c5c',
+        '--content-background': '#797979',
+        '--content-border': '#696969',
+        '--navbar-button-color': 'rgb(85, 80, 144)',
+        '--navbar-button-border': 'rgba(53, 28, 112, 0.301)',
+        '--navbar-button-hover-color': 'rgb(147, 143, 192)',
+        '--navbar-button-hover-border': 'rgba(182, 178, 192, 0.301)',
+        '--font-color': '#f1f1f1',
+        '--icon-color': '#f1f1f1',
     }
 };
 
