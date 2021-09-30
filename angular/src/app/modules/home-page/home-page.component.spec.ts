b41d968e0a1c9d1288356c789daf0a77b45c9272
DuglasColoma <duglas.espanol@gmail.com>
2021-09-30 17:27:45 -0300

    fixing a small error

--- angular/src/app/modules/home-page/home-page.component.spec.ts
+++ angular/src/app/modules/home-page/home-page.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateTestingModule } from 'ngx-translate-testing';
 import { HomePageComponent } from './home-page.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from 'src/app/shared-module';
+import { MaterialModule } from '../ui/material.module';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
@@ -9,10 +14,17 @@ describe('HomePageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ HomePageComponent ],
-      imports : [ TranslateTestingModule.withTranslations({})],
+      imports: [
+        RouterTestingModule,
+        CommonModule,
+        MaterialModule,
+        HttpClientModule,
+        SharedModule,
+        TranslateTestingModule.withTranslations({})
+      ],
       providers: []
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
